28ace882d481e90eb3f8eb0fac98dc2b0eb41443
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-01-31 14:40:58 +0000

    1/31/25

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
+// Import the ArrayList CLASS
+// (from the java.util PACKAGE)
+import java.util.ArrayList;
+
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // DECLARE an ArrayList object
+      // Type <> is required, each ArrayList can hold one type of object
+      ArrayList<String> buzzwords; 
+
+      // DECLARE & CREATE an ArrayList
+      // by calling the CONSTRUCTOR method
+      ArrayList<String> shoppingList = new ArrayList<String>();
+
+      // ArrayList can ONLY store OBJECTS!! 
+      // so for int and double (primitive types)
+      // you must use their WRAPPER CLASSES
+      // Integer ad Double "wrap" primitives into OBJECTS
+      // objects have methods, primitives don't 
+      ArrayList<Integer> ages = new ArrayList<Integer>();
+      ArrayList<Double> testScore = new ArrayList<Double>();
+      // Java will handle the "wrapping" for you, 
+      // this is called AUTOBOXING
+
+      // Use the .add( obj ) METHOD to populate/fi;; list
+      testScore.add(40.00);
+      System.out.println(testScore); // prints [40.0]
    }
 }
